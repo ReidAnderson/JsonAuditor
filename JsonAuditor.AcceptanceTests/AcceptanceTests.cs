b9d90ec257acbb48ea5ca4223dbc11faf6b3f2d0
ReidAnderson <rbarryanderson@gmail.com>
2021-03-14 20:24:39 -0500

    More consolidation

--- JsonAuditor.AcceptanceTests/AcceptanceTests.cs
+++ JsonAuditor.AcceptanceTests/AcceptanceTests.cs
@@ -70,15 +70,7 @@ namespace JsonAuditor.AcceptanceTests
                 Entity = "{\"something\":\"here\"}"
             };
 
-            await _client.PostAsJsonAsync("/Audit", request);
-
-            var response = await _client.GetAsync($"/Audit?entityId={entityId}&entityType=0");
-            Debug.WriteLine(JsonSerializer.Serialize(response));
-            
-            Assert.True(response.IsSuccessStatusCode);
-
-            string output = await response.Content.ReadAsStringAsync();
-            Assert.True(request.Entity == output);
+            UpdateAndCheckOutput(request);
 
             request = new AuditRequest() {
                 UniqueIdentifier = new Guid().ToString(),
@@ -88,18 +80,10 @@ namespace JsonAuditor.AcceptanceTests
                 Entity = "{\"something\":\"more\",\"anything\":\"else\"}"
             };
 
-            await _client.PostAsJsonAsync("/Audit", request);
+            UpdateAndCheckOutput(request);
 
-            response = await _client.GetAsync($"/Audit?entityId={entityId}&entityType=0");
-            Debug.WriteLine(JsonSerializer.Serialize(response));
-            
-            Assert.True(response.IsSuccessStatusCode);
-
-            output = await response.Content.ReadAsStringAsync();
-            Assert.True(request.Entity == output);
-
-            response = await _client.GetAsync($"/Audit?entityId={entityId}&entityType=0&auditTime={request.TransactionDateTime.AddHours(-2)}");
-            output = await response.Content.ReadAsStringAsync();
+            var response = await _client.GetAsync($"/Audit?entityId={entityId}&entityType=0&auditTime={request.TransactionDateTime.AddHours(-2)}");
+            var output = await response.Content.ReadAsStringAsync();
             Assert.True(output == "{\"something\":\"here\"}");
         }
     }
